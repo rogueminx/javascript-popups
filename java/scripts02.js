f689bef23cbd923b9a86fc4b7250e2be34d7e12a
Anna-Marie Cooper <annamarie@rogueminx.com>
2017-10-11 13:01:31 -1000

    add fadeToggle and fadeSlide to peekaboohtml

--- java/scripts02.js
+++ java/scripts02.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
     $("#initially-showing").toggle();
     $("#initially-hiding").toggle();
   });
+  $(".clickable02").click(function() {
+    $("#initially-showing02").fadeToggle();
+    $("#initially-hiding02").fadeToggle();
+  });
+  $(".clickable03").click(function() {
+    $("#initially-showing03").slideToggle();
+    $("#initially-hiding03").slideToggle();
+  });
 });
